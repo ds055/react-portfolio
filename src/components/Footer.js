0a14c6c2078e979500257c79b401eeeaf4ed4b35
Drew Smith <drgh056@gmail.com>
2023-07-14 20:51:33 -0500

    Created component files. Added Bootstrap NPM and FontAwesome NPM

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+// return Footer el
+export default function Footer() {
+    return (
+        <Container>
+            <Row>
+            <h1>Greebert Devs</h1>
+            </Row>
+            <Row>
+
+            </Row>
+        </Container>
+);
+}
